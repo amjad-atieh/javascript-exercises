10f8c9fcbefe71942309c4a4c230e8570fa2ce64
Amjad <amjdsalh@gmail.com>
2025-07-21 15:11:17 +0300

    finshed excersice 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num)
+{
+    if (num < 0)
+        return 'ERROR';
+    let strCpy = '';
 
+    for (let i = 0; i < num; i++)
+    {
+        strCpy += str;
+    }
+    return strCpy;
 };
 
 // Do not edit below this line
