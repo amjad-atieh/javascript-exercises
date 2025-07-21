75ffe4caf4f51d50252109e9523e7ac5a377eae1
Amjad <amjdsalh@gmail.com>
2025-07-21 15:32:02 +0300

    finshed excersice 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr)
+{
+    let i;
+
+    for (let j = 1; j < arguments.length; j++)
+    {
+        i = 0;
+        while (i < arr.length)
+        {
+            if (arr[i] === arguments[j])
+                arr.splice(i, 1);
+            else
+                i++;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
