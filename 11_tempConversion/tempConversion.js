aa3e1247357deeb0b1eafe3d961573b7d093ad8c
Amjad <amjdsalh@gmail.com>
2025-07-21 15:45:49 +0300

    finshed excersice 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit)
+{
+  const celsius = 5/9 * (fahrenheit - 32);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius)
+{
+  const fahrenheit = (9/5 * celsius) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
