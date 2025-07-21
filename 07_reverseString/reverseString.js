9294eabb6d2c3144c1298f4904da000e5f61ebe7
Amjad <amjdsalh@gmail.com>
2025-07-21 15:14:47 +0300

    finshed excersice 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str)
+{
+    let charArr = [];
 
+    for (let i = str.length - 1; i >= 0; i--)
+    {
+        charArr.push(str[i]);
+    }
+    return (charArr.join(''));
 };
 
 // Do not edit below this line
