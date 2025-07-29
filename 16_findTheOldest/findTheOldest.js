9456cf80fd0e32a2cb6c9d95a077714cc8acc366
Amjad <amjdsalh@gmail.com>
2025-07-30 01:20:41 +0300

    exercise 16 done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    oldestPerson = people.reduce(function(oldestPerson, person) {
+        if (typeof(person.yearOfDeath) == "undefined")
+            person.yearOfDeath = 2025;
+        if (oldestPerson == "" || oldestPerson.age < person.yearOfDeath - person.yearOfBirth)
+        {
+            oldestPerson = {name: person.name, age: person.yearOfDeath - person.yearOfBirth};
+        }
+        return oldestPerson;
+    }, "");
+    return (oldestPerson);
 };
 
 // Do not edit below this line
