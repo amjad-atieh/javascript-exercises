9e8ef127cafb7d2c593b8fecc3e4af6c6bd29c61
Amjad <amjdsalh@gmail.com>
2025-07-30 00:04:52 +0300

    exercise 13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
-
+const palindromes = function (line) {
+    line = line.toLowerCase();
+    let left = 0; right = line.length - 1;
+    while (left < right)
+    {
+        while (isPunctuation(line[left]))
+            left++;
+        while (isPunctuation(line[right]))
+            right--;
+        if (left >= right)
+            break;
+        if (line[right] != line[left])
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
+
+function isPunctuation(letter)
+{
+    if (letter == ' ' || letter == '.' || letter == '!' || letter == '?' || letter == ',')
+        return true;
+    else
+        return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
