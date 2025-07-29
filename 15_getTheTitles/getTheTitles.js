cc3f3ab9ae033579ccc62ba8714a651b0c0ee536
Amjad <amjdsalh@gmail.com>
2025-07-30 00:54:54 +0300

    exercise 15 done

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objArr) {
+    return objArr.reduce(function(titleArr, book, index) {
+        titleArr[index] = book.title;
+        return titleArr;
+    }, []);
 };
 
 // Do not edit below this line
