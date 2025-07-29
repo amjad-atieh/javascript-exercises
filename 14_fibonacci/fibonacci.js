63781059c2d07a1f4b4eca96d8abce087e7032ef
Amjad <amjdsalh@gmail.com>
2025-07-30 00:51:15 +0300

    exercise 14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
 
+
+const fibonacci = function (num)
+{
+    if (num < 0)
+        return "OOPS";
+    let fibonacciArray = [0, 1];
+
+    while (fibonacciArray.length <= num)
+    {
+        fibonacciArray[fibonacciArray.length] = fibonacciArray[fibonacciArray.length - 1]
+            + fibonacciArray[fibonacciArray.length - 2];
+    }
+    return (fibonacciArray[num]);
 };
 
 // Do not edit below this line
